b7212dd2f2e6f139a632bfd44817a728e3cde11f
spindling <jeremy.sean009@gmail.com>
2025-11-15 19:43:24 -0500

    add starter files containing empty classes

---
+++ API_facade.py
@@ -0,0 +1,14 @@
+# Facade pattern used to simplify API access
+
+class QueryReleases:
+
+    def search_releases():
+        pass
+
+    def select_match():
+        pass
+
+class FormatEntry:
+    
+    def format_metadata():
+        pass
---
+++ mvc.py
@@ -0,0 +1,59 @@
+# Model-View-Controller pattern for main interface 
+
+class Model:
+    
+    def __init__(self):
+        pass
+
+    def create_report():
+        pass
+
+    def retrieve_report_data():
+        pass
+
+    def retrieve_metadata():
+        pass
+
+    def add_to_database():
+        pass
+
+    def delete_from_database():
+        pass
+
+
+class Controller:
+    def __init__(self):
+        pass
+        
+    def start():
+        pass
+
+    def modify_database():
+        pass
+
+    def retrieve_from_database():
+        pass
+
+    def generate_report():
+        pass
+
+
+class View:
+
+    def __init__(self):
+        pass
+    
+    def menu():
+        pass
+    
+    def add_entry():
+        pass
+
+    def delete_entry():
+        pass
+
+    def show_entries():
+        pass
+
+    def request_report():
+        pass
